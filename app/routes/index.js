eb6538394d0b3d25c09d606e2083104c1f6eafc9
Brad London <london.brad@gmail.com>
2016-03-25 14:27:25 -0700

    finish hw but can not add a listing

---
+++ app/routes/index.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+
+
+export default Ember.Route.extend({
+  model() {
+    return this.store.findAll('rental');
+  },
+
+  actions: {
+    save3(params) {
+      var newRental = this.store.createdRecord('rental', params);
+      newRental.save();
+      this.transitionTo('index');
+    },
+    
+    destroyRental(rental) {
+      rental.destroyRecord();
+      this.transitionTo('index');
+    }
+  }
+});
